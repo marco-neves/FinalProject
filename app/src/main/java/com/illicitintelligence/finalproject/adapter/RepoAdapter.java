09019bfee64d03a8e4384b89e1b3d40578a25abd
User <user@Users-Macintosh.local>
2020-01-28 16:54:36 -0500

    adapters and itemview started and populated

--- app/src/main/java/com/illicitintelligence/finalproject/adapter/RepoAdapter.java
+++ app/src/main/java/com/illicitintelligence/finalproject/adapter/RepoAdapter.java
@@ -1,33 +1,46 @@
 package com.illicitintelligence.finalproject.adapter;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class RepoAdapter extends RecyclerView.Adapter<RepoAdapter.RepoViewHolder>{
+import com.illicitintelligence.finalproject.R;
+import com.illicitintelligence.finalproject.model.RepoResult;
 
+import java.util.List;
+
+public class RepoAdapter extends RecyclerView.Adapter<RepoAdapter.RepoViewHolder>{
+    private List<RepoResult> repoResults;
 
     @NonNull
     @Override
     public RepoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.project_item_layout, parent, false);
+
+        return new RepoViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull RepoViewHolder holder, int position) {
-
+        //holder.projectTitleTextview.setText(repoResults.get(position).);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return repoResults.size();
     }
 
     class RepoViewHolder extends RecyclerView.ViewHolder {
+        TextView projectTitleTextview;
         public RepoViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            projectTitleTextview = itemView.findViewById(R.id.project_title_textview);
         }
     }
 }
